fc84445ffd36c309f754a3d5b96668b2cc1ff1e8
teknoboten <serra@boten.ca>
2023-06-16 14:37:53 -0700

    Revert "include span count in summaries (#1)"
    This reverts commit bb8af43f621b299116e0d77ed5e283bf6a1fcaf4.

--- desktopexporter/internal/telemetry/telemetry_data.go
+++ desktopexporter/internal/telemetry/telemetry_data.go
@@ -1,51 +1,17 @@
 package telemetry
 
-import (
-	"time"
-)
-
 func (t *TelemetryData) GetSummary() TelemetrySummary {
-
 	switch t.Type {
 	case "trace":
 		traceSummary := t.Trace.GetTraceSummary()
-		rootSpan, err := t.Trace.getRootSpan()
-
-		if err == WarningMissingRootSpan {
-			return TelemetrySummary{
-				HasRootSpan:     false,
-				RootServiceName: "",
-				RootName:        "",
-				RootStartTime:   time.Time{},
-				RootEndTime:     time.Time{},
-				SpanCount:       uint32(len(t.Trace.Spans)),
-				ID:              traceSummary.TraceID,
-				Type:            t.Type,
-				ServiceName:     "",
-			}
-		}
-
 		return TelemetrySummary{
-			HasRootSpan:     true,
-			RootServiceName: rootSpan.GetServiceName(),
-			RootName:        rootSpan.Name,
-			RootStartTime:   rootSpan.StartTime,
-			RootEndTime:     rootSpan.EndTime,
-			SpanCount:       uint32(len(t.Trace.Spans)),
-			ID:              traceSummary.TraceID,
-			Type:            t.Type,
+			Type:        t.Type,
+			ID:          traceSummary.TraceID,
+			ServiceName: traceSummary.RootServiceName,
 		}
 	}
-
 	return TelemetrySummary{
-		HasRootSpan:     false,
-		RootServiceName: "", //maybe we can omit these?
-		RootName:        "",
-		RootStartTime:   time.Time{},
-		RootEndTime:     time.Time{},
-		ServiceName:     "",
-		SpanCount:       0,
-		ID:              t.ID,
-		Type:            t.Type,
+		Type: t.Type,
+		ID:   t.ID,
 	}
 }
